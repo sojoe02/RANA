f9108b598876969ddb8b76f4b7b88539133ebe73
sojoe02 <sojoe02@gmail.com>
2014-08-22 13:03:45 +0200

    added license information to all files

--- lua_agents/data_collector_demo.lua
+++ lua_agents/data_collector_demo.lua
@@ -1,3 +1,24 @@
+----begin_license--
+--
+--Copyright 	2013 	Søren Vissing Jørgensen.
+--			2014	Søren Vissing Jørgensen, Center for Bio-Robotics, SDU, MMMI.  
+--
+--This file is part of RANA.
+--
+--RANA is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--RANA is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with RANA.  If not, see <http://www.gnu.org/licenses/>.
+--
+----end_license--
 -- Global variables:
 posX = 0 	--autons X position
 posY = 0 	--autons Y position
