cac56252379e3a07c75cb14bc4a40e17bde110ef
sojoe02 <sojoe02@gmail.com>
2014-08-22 13:43:12 +0200

    fixed licenses not properly applied

--- agentengine/agents/autonLUA.h
+++ agentengine/agents/autonLUA.h
@@ -19,27 +19,7 @@
 //along with RANA.  If not, see <http://www.gnu.org/licenses/>.
 //
 //--end_license--
-//--begin_license--
-//
-//Copyright 	2013 	Søren Vissing Jørgensen.
-//			2014	Søren Vissing Jørgensen, Center for Biorobotics, Sydansk Universitet MMMI.  
-//
-//This file is part of RANA.
-//
-//RANA is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
-//
-//RANA is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with RANA.  If not, see <http://www.gnu.org/licenses/>.
-//
-//--end_license--
+
 #ifndef AUTONLUA_H
 #define AUTONLUA_H
 
