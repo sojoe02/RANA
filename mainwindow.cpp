440be96395c85589cc8b415d37ad2585060ff1db
sojoe02 <sojoe02@gmail.com>
2014-11-07 14:50:38 +0100

    Small interface corrections

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -80,7 +80,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QObject::connect(ui->action_Exit, SIGNAL(triggered()),this, SLOT(actionExit()));
     QObject::connect(ui->action_Info, SIGNAL(triggered()),this, SLOT(actionPrintInfo()));
 
-	versionString = QString("<b><font color=\"green\">RANA</b></font> version 1.2.7:0.4.1");
+	versionString = QString("<b><font color=\"green\">RANA</b></font> version 1.2.7:0.4.2");
 
 	ui->statusBar->addWidget(new QLabel(versionString));
 	ui->graphicsView->setDragMode(QGraphicsView::ScrollHandDrag);
