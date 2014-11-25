ef02099ea32fafa21a8ff978b03b1d7ba6ec7569
sojoe02 <sojoe02@gmail.com>
2014-11-25 13:37:00 +0100

    Made the event time interval spinboxes wrap, same with the event map spinbox

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -80,7 +80,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QObject::connect(ui->action_Exit, SIGNAL(triggered()),this, SLOT(actionExit()));
     QObject::connect(ui->action_Info, SIGNAL(triggered()),this, SLOT(actionPrintInfo()));
 
-	versionString = QString("<b><font color=\"green\">RANA</b></font> version 1.2.10:0.5.0");
+	versionString = QString("<b><font color=\"green\">RANA</b></font> version 1.2.10:0.5.1");
 
 	ui->statusBar->addWidget(new QLabel(versionString));
 	ui->graphicsView->setDragMode(QGraphicsView::ScrollHandDrag);
