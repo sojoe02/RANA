3f9de50d7db7403e8d6133387e701987b83f6fcd
Theis <rorissian@hotmail.com>
2018-01-23 14:37:20 +0100

    Continiued work on Lizard ear.

--- src/lua_agents/17_lizard.lua
+++ src/lua_agents/17_lizard.lua
@@ -26,11 +26,45 @@ Event = require "ranalib_event"
 Shared = require "ranalib_shared"
 Stat = require "ranalib_statistic"
 
+sample_rate = 50000;
+target_freq = 2300;
+ear_separation = 13*1500/target_freq;
+sf = ear_separation/13;
+
+sound_freq = 2000;
+omega = 2*math.pi*sound_freq;
+c = omega*13/340000;
+
+n_samples = 500;
+n_op_samples = 50;
+
 function _InitializeAgent()
         say("Agent #: " .. ID .. " has been initialized")
+
+        PositionY = -100
+        PositionX = -100
+
+        x = 300
+        y = 300
+
+        dist = math.sqrt(math.pow(y-PositionY,2) + math.pow(x-PositionX,2));
+        spkr_angle = math.atan2(y-PositionY,x-PositionX)
+
+        --  Sense the sound direction with the ear model
+        phase_diff = c*math.sin(0-spkr_angle);
+        sinewave_L = math.sin(omega*t - 0);
+        sinewave_R = math.sin(omega*t - phase_diff);
+
+        print(dist .. " " .. spkr_angle .. " " .. phase_diff)
+
 end
 
-function HandleEvent(event)
+function handleEvent(sourceX, sourceY, sourceID, eventDescription, eventTable)
+
+        local event_table = {x=sourceX, y=sourceY}
+
+        Event.emit{targetID=sourceID, speed=343, description="echo", table=event_table}
+
 end
 
 function takeStep()
