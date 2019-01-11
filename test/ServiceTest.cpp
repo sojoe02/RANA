220e491169dc051eea4cfaec36da91ad4cd7898d
sojoe <sojoe02@gmail.com>
2019-01-11 18:37:30 +0100

    Refactor of class file names, and added unit test library

---
+++ test/ServiceTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sojoe on 1/10/19.
+//
+
+#include "ServiceTest.h"
---
+++ test/ServiceTest.h
@@ -0,0 +1,15 @@
+//
+// Created by sojoe on 1/10/19.
+//
+
+#ifndef RANA_SERVICETEST_H
+#define RANA_SERVICETEST_H
+
+
+class ServiceTest {
+
+
+};
+
+
+#endif //RANA_SERVICETEST_H
